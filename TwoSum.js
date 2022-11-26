239cf14ff48b1867741d146bcc82927eeea4218d
Sagar Kapoor <sagrkapr@gmail.com>
2022-11-26 09:53:31 +0200

    ✅ Two Sum problem solved with forEach

---
+++ TwoSum.js
@@ -0,0 +1,24 @@
+/*Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+const twoSum = (nums, target) => {
+  let a;
+  let b;
+  nums.forEach((num, index) => {
+    nums.forEach((number, secondIndex) => {
+      if (index !== secondIndex) {
+        if (num + number === target) {
+          a = index;
+          b = secondIndex;
+        }
+      }
+    });
+  });
+  console.log([b, a]);
+};
+
+twoSum([2, 7, 11, 15], 9);
