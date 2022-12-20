01f8cd2ae96378445e2c2bd25bec0c67ec0b8e45
Sagar Kapoor <sagrkapr@gmail.com>
2022-12-20 10:40:13 +0200

    ✅ Solution for Add Two linked lists

---
+++ addTwoNumbers.js
@@ -0,0 +1,42 @@
+/*You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example 1:
+
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807
+*/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = (l1, l2) => {
+  let carry = 0;
+  let result = new ListNode(0);
+  let current = result;
+
+  while (l1 !== null || l2 !== null || carry !== 0) {
+    let val1 = l1 !== null ? l1.val : 0;
+    let val2 = l2 !== null ? l2.val : 0;
+    let sum = val1 + val2 + carry;
+    carry = sum >= 10 ? 1 : 0;
+    current.next = new ListNode(sum % 10);
+    current = current.next;
+    if (l1 !== null) l1 = l1.next;
+    if (l2 !== null) l2 = l2.next;
+  }
+
+  return result.next;
+};
